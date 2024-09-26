15d68477ccbc0ffb2f3025b10b0cb1b297e31fa1
Gabriel Bertele <gabrielbertele@gmail.com>
2024-09-26 20:28:19 -0300

    primeiro commit

---
+++ DjangoTcheOrienta/DjangoTcheOrienta/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nome_do_seu_banco_de_dados',
+        'USER': 'seu_usuario_mysql',
+        'PASSWORD': 'sua_senha_mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
